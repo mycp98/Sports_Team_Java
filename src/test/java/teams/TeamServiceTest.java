1cd5c14185b8d08c142cc72ed2095cce865f9deb
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-02-07 19:50:05 +0000

    TeamService passed test

--- src/test/java/teams/TeamServiceTest.java
+++ src/test/java/teams/TeamServiceTest.java
@@ -1,4 +1,48 @@
 package teams;
 
+import managers.Manager;
+import org.junit.jupiter.api.Test;
+import players.Player;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class TeamServiceTest {
+    @Test
+    public void canCountEmptySpacesOnTeamWhenEmpty(){
+        //Given
+        Manager manager = new Manager("test manager");  //need to create the manager object before passing it onto the team
+        Team team = new Team("team1", manager);
+        TeamService underTest = new TeamService();
+        //When
+        int actual = underTest.countEmptySpacesOnTeam(team);
+        //Then
+        int expected = 15;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void canCountEmptySpacesOnTeamWithOnePlayer(){
+        //Given
+        Manager manager = new Manager("test manager");
+        Team team = new Team("Test RFC", manager);
+        Player player = new Player("test player", "winger", 11, 2);
+        TeamService underTest = new TeamService();
+
+        team.getPlayers()[0] = player;
+
+//        try {
+//            underTest.addPlayerToTeam(team, player);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        //When
+        int actual = underTest.countEmptySpacesOnTeam(team);
+
+        //Then
+        int expected =14;
+        assertThat(actual).isEqualTo(expected);
+
+    }
 }
