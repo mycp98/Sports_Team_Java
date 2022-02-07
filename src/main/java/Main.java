dc9a24e71cda2900ace11e683d3bdd8632b75645
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-02-07 11:40:45 +0000

    created Player pojo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
+import POJOs.Player;
+
 public class Main {
+    public static void main(String[] args) {
+        Player player = new Player("Finn Russell", "Fly Half", 10, 10);
+
+        System.out.println(player);
+    }
 }
---
+++ src/main/java/POJOs/Player.java
@@ -0,0 +1,73 @@
+package POJOs;
+
+import java.util.Objects;
+
+public class Player {
+    // Properties
+    private String name;
+    private String position;
+    private int number;
+    private int contractLength;
+
+    //Constructor
+    public Player(String name, String position, int number, int contractLength){
+        this.name = name;
+        this.position = position;
+        this.number = number;
+        this.contractLength = contractLength;
+    }
+    //default constructor
+    public Player(){
+
+    }
+    //Encapsulation - to access the information we will need to call the method
+    //Getters:
+    public String getName(){
+        return name;
+    }
+    public String getPosition(){
+        return position;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public int getContractLength(){
+        return contractLength;
+    }
+    //Setters:
+    public void setName(String name){    //void because we don't want anything back
+        this.name = name;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public void setContractLength(int contractLength){
+        this.contractLength = contractLength;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", number=" + number +
+                ", contractLength=" + contractLength +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return number == player.number && contractLength == player.contractLength && Objects.equals(name, player.name) && Objects.equals(position, player.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position, number, contractLength);
+    }
+}
